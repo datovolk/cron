a7b39a877ee5cdbe02242a0d40241d00120dc7e1
datovolk <38498446+datovolk@users.noreply.github.com>
2025-07-11 00:42:31 +0400

    s

--- selenium_config.py
+++ selenium_config.py
@@ -9,13 +9,14 @@ def get_driver(user_agent=None, headless=True):
     options = webdriver.ChromeOptions()
     if headless:
         options.add_argument('--headless')
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--start-maximized")
     options.add_argument(f"user-agent={user_agent}")
-
+    options.binary_location = "/usr/bin/google-chrome"
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    driver_path = os.path.join(base_dir,  "chromedriver")
+    driver_path = os.path.join(base_dir, "chromedriver")
     driver_path = os.path.abspath(driver_path)
-
     service = Service(driver_path)
 
     driver = webdriver.Chrome(service=service, options=options)
